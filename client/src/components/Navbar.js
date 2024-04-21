683815fe1925991f1fd3079213af0bce14a8ea73
dl67583 <dl67583@ubt-uni.net>
2024-04-21 15:33:27 +0200

    navbar and footer

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container container">
+        <input type="checkbox" name="" id="" />
+        <div className="hamburger-lines">
+          <span className="line line1"></span>
+          <span className="line line2"></span>
+          <span className="line line3"></span>
+        </div>
+        <ul className="menu-items">
+          <li><a href="/users">Users</a></li>
+          <li><a href="/departments">Departments</a></li>
+          <li><a href="/candidates">Candidates</a></li>
+          <li><a href="/meetings">Meetings</a></li>
+        </ul>
+        <h1 className="logo">Navbar</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
