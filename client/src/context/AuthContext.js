0301b4f40c42a308f477e537d99f0e2299425af9
Adhurim <adhurimdana1@gmail.com>
2024-06-03 19:26:42 +0200

    added AuthContext.js for login and logout

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,42 @@
+// src/context/AuthContext.js
+import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      axios.post('/api/verify-token', {}, { headers: { Authorization: `Bearer ${token}` } })
+        .then(response => {
+          if (response.status === 200) {
+            setIsAuthenticated(true);
+          }
+        })
+        .catch(() => {
+          setIsAuthenticated(false);
+        });
+    }
+  }, []);
+
+  const login = (token) => {
+    localStorage.setItem('token', token);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
