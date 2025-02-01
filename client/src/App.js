7d97c0adae2c7fba9d10caad7c7f244a852c1681
driterolulaj <driterolulaj@gmail.COM>
2025-02-01 17:33:22 +0100

    fixed sidebar 77.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 // App.js
-import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { useAuth } from './context/AuthContext';  // Ensure the path is correct
-import { SidebarProvider, useSidebar } from './context/sidebarContext';
-import Sidebar from './components/Sidebar';
-import Navbar from './components/Navbar';
-import './styles/App.css';
-import Login from './routes/Login';  // Ensure the path is correct
-import Home from './routes/Home';  // Ensure the path is correct
-import Users from './routes/Users';  // Ensure the path is correct
-import Dashboard from './routes/Dashboard';  // Ensure the path is correct
-import Departments from './routes/Departments';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useAuth } from "./context/AuthContext"; // Ensure the path is correct
+import { SidebarProvider, useSidebar } from "./context/sidebarContext";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import "./styles/App.css";
+import Login from "./routes/Login"; // Ensure the path is correct
+import Home from "./routes/Home"; // Ensure the path is correct
+import Users from "./routes/Users"; // Ensure the path is correct
+import Dashboard from "./routes/Dashboard"; // Ensure the path is correct
+import Departments from "./routes/Departments";
 
 const AppContent = () => {
   // const { user, authLoading } = useAuth();
@@ -21,19 +21,21 @@ const AppContent = () => {
   // }
 
   return (
-    <div className={`app-container content ${isSidebarOpen ? "sidebar-open" : ""}`}>
+    <div
+      className={`app-container content ${isSidebarOpen ? "sidebar-open" : ""}`}
+    >
       <Sidebar />
       <Navbar />
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/departments" element={<Departments />} />
-        <Route path="/" element={<Home />} />
-
-      </Routes>
+      <div className="main">
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/departments" element={<Departments />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 };
-
 
 export default AppContent;
